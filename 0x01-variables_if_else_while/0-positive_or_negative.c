7742454739b36221cea39c9a27e2253c77cc9505
Ahmed Abdul-mageed <66303021+B4bunna@users.noreply.github.com>
2023-08-13 17:24:49 +0200

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-Printf("%d is positive\n, n");
+Printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-Printf("%d is zero\n, n");
+Printf("%d is zero\n", n);
 }
 else
 {
-printf("%d is negative\n, n");
+printf("%d is negative\n", n);
 }
 return (0);
 }
