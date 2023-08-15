3f725849f16117e4afdd9446e41734840a5eec64
B4unna <ahmedmageed24@gmil.com>
2023-08-15 17:04:01 +0200

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  main - a function that prints the alphabet, in lowercase, followed by a new line.
+ *   Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+int n = 97;
+while (n <= 122)
+{
+_putchar(n);
+n++;
+}
+_putchar(\n);
+}
