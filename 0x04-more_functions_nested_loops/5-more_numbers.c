e9b531cbab5b4540ba7a81093b46c48bba5b6d56
B4bunna <ahmedmageed24@gmail.com>
2023-08-18 23:18:54 +0200

    get lost in the middle of this

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - entry point
+ * description: a function that prints 10 times the numbers, from 0 to 14, followed by a new line.
+ * return: num
+ */
+
+void more_numbers(void)
+{
+int x;
+int y;
+
+for (x = 0; x < 10; x++)
+{
+for (y = 0; y <= 14; y++)
+{
+if (y > 9)
+
+}
+}
+}
