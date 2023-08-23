22c5da8b1185cb56c885e6530ba4af4e7ecc196a
B4bunna <ahmedmageed24@gmail.com>
2023-08-23 12:28:14 -0700

    hi

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * main - entry point
+ * description: concatenates two strings.
+ * return: void
+ *
+ * */
+
+char *_strcat(char *dest, char *src)
+{
+int i,j;
+
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+j = 0;
+while (src[j] != '\0')
+{
+dest[i] = src [j];
+i++;
+j++;
+}
+dest[i] = '\0'
+return (dst);
+}
