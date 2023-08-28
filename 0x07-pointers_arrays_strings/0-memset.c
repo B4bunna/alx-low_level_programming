f8816d4ca5bba9cf03f4ac9ddae624f3a904f1da
B4bunna <ahmedmageed24@gmail.com>
2023-08-28 15:09:49 +0400

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * description: function that fills memory with a constant byte
+ * Return: char with result of memset
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
