6fb653a80ecc719231c9448f57ca0f4afbd11baa
B4bunna <ahmedmageed24@gmail.com>
2023-08-28 15:37:33 +0400

    5 in rows

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * description: function that copies memory area
+ * Return: char with result of memset
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * description: function that locates a character in a string
+ * Return: char with result
+ */
+char *_strchr(char *s, char c)
+{
+
+	if (*s == '\0')
+		return (s);
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	       s++;
+	}
+	if (c == '\0')
+		return (s);
+	return ('\0');
+}
