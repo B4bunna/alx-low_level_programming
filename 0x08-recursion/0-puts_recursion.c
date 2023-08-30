776d6db8b303957d4551d7e933babaede382baf5
B4bunna <ahmedmageed24@gmail.com>
2023-08-30 22:20:10 +0400

    hi recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * description: prints a string followed by a new line
+ * Return: On success 1. On error -1 
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
