d014867790643ef85f8a78e8bc4b7a4fbe6da178
B4bunna <ahmedmageed24@gmail.com>
2023-08-31 22:23:24 +0400

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,8 +9,8 @@ int sqrt2(int a, int b)
 if (b * b == a)
 	return (b);
 else (b * b > a)
-	return -1;
-return (sqrt2(a,  + 1));
+	return (-1);
+return (sqrt2(a, b + 1));
 }
 
 int _sqrt_recursion(int n)
